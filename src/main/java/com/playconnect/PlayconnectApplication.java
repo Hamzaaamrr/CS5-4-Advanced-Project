d4010399c6753fa7c35176dd267a9bdaeba4e54e
Hamza Amr <hamzaamreladawy@gmail.com>
2026-03-25 21:53:43 +0200

    Firt Commit (SpringBoot empty project)

---
+++ src/main/java/com/playconnect/PlayconnectApplication.java
@@ -0,0 +1,13 @@
+package com.playconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayconnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayconnectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/playconnect/playconnect/PlayconnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.playconnect.playconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayconnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
