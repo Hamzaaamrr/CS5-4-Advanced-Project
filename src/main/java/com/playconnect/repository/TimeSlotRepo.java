4b334eb89f59d98e5505f51325f26ed1ef183652
malakafifi <malak21afifi@gmail.com>
2026-04-21 20:17:13 +0200

    first commit

--- src/main/java/com/playconnect/repository/TimeSlotRepo.java
+++ src/main/java/com/playconnect/repository/TimeSlotRepo.java
@@ -1,11 +1,18 @@
 package com.playconnect.repository;
 
 import com.playconnect.entity.TimeSlot;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
 public interface TimeSlotRepo extends JpaRepository<TimeSlot, Long>{
-    
+    List<TimeSlot> findByCourtIdAndDateOrderByStartTimeAsc(Long courtId, LocalDate date);
+    Optional<TimeSlot> findByCourtIdAndDateAndStartTimeAndEndTime(Long courtId, LocalDate date, LocalTime startTime, LocalTime endTime);
 }
